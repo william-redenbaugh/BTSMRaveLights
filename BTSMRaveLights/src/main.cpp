f6b3d3b5291c9916bae39f48af05080fa88d08dd
William Redenbaugh <wmredenbaugh@gmail.com>
2023-07-16 23:37:16 -0700

    moved to different FFT algo, other stuff I guess heh

--- BTSMRaveLights/src/main.cpp
+++ BTSMRaveLights/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include "fix_fft.h"
+#include "arduinoFFT.h"
 #include <driver/i2s.h>
 #include "FastLED.h"
 
@@ -7,7 +7,7 @@
 #define FFT_SIZE 512
 #define POW 8
 
-#define DIVIDOR 512
+#define DIVIDOR 3000000
 
 // you shouldn't need to change these settings
 #define SAMPLE_BUFFER_SIZE NUM_SAMPLES
@@ -21,9 +21,9 @@
 #define I2S_MIC_SERIAL_DATA_CS GPIO_NUM_7
 
 SemaphoreHandle_t fft_mic_data_mtx = NULL;
-char mic_data_fft_copy[FFT_SIZE], im[FFT_SIZE];  // FFT Array Variables
-char fft_inplace_buffer[FFT_SIZE];
-char data_out[FFT_SIZE];
+double mic_data_fft_copy[FFT_SIZE], im[FFT_SIZE];  // FFT Array Variables
+double fft_inplace_buffer[FFT_SIZE];
+double data_out[FFT_SIZE];
 
 SemaphoreHandle_t fft_animation_copy_mtx = NULL;
 uint8_t fft_data_animation_copy[FFT_SIZE];
@@ -64,7 +64,7 @@ i2s_pin_config_t i2s_mic_pins = {
 int32_t raw_samples[SAMPLE_BUFFER_SIZE];
 
 CRGB leds[NUMPIXELS];
-
+arduinoFFT FFT = arduinoFFT();  // Create FFT object
 
 void setup() {
   // we need serial output for the plotter
@@ -115,21 +115,28 @@ void fft_thread(void *parameters){
     xSemaphoreTake(fft_mic_data_mtx, portMAX_DELAY);
     for(int i = 0; i < NUM_SAMPLES; i++){
       fft_inplace_buffer[i] = mic_data_fft_copy[i];
+      im[i] = 0;
     }
     xSemaphoreGive(fft_mic_data_mtx);
 
-    fix_fft((int8_t*)fft_inplace_buffer, (int8_t*)im, 8, 0);
+    FFT.Windowing(fft_inplace_buffer, FFT_SIZE/2, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(fft_inplace_buffer, im, FFT_SIZE/2, FFT_FORWARD);
+    FFT.ComplexToMagnitude(fft_inplace_buffer, im, FFT_SIZE);
 
     for(int i = 0; i < NUM_SAMPLES; i++){
       data_out[i] = fft_inplace_buffer[i];
     }
 
+
+
     xSemaphoreTake(fft_animation_copy_mtx, portMAX_DELAY);
     for (int i = 0; i < NUM_SAMPLES; i++) // only need N/2 elements of the array
     {
-      fft_data_animation_copy[i] = abs(data_out[i]);
+      fft_data_animation_copy[i] = uint8_t( data_out[i] / 13000000);
     }
     xSemaphoreGive(fft_animation_copy_mtx);
+
+    delay(15);
   }
 }
 
@@ -138,14 +145,14 @@ void led_matrix_thread(void *parameters){
   FastLED.setBrightness(90);
   for(;;){
     xSemaphoreTake(fft_animation_copy_mtx, portMAX_DELAY);
-    for(int n = 0; n < NUM_SAMPLES; n++){
+    for(int n = 0; n < NUMPIXELS; n++){
       leds[n].setHSV(fft_data_animation_copy[n], 255,  fft_data_animation_copy[n]);
     }
     xSemaphoreGive(fft_animation_copy_mtx);
 
     FastLED.show();
 
-    delay(30);
+    delay(15);
   }
 }
 
@@ -157,22 +164,9 @@ void mic_thread(void *parameters){
     // BLOCKING READ to get all data
     i2s_read(I2S_NUM_0, raw_samples, sizeof(int32_t) * SAMPLE_BUFFER_SIZE, &bytes_read, portMAX_DELAY);
     int samples_read = bytes_read / sizeof(int32_t);
-
-    for(int i = 0; i < NUM_SAMPLES; i++){
-      raw_samples[i] = raw_samples[i] / DIVIDOR;
-      if(raw_samples[i] > 127){
-        //raw_samples[i] = 127;
-      }
-
-      if(raw_samples[i] < -127){
-        //raw_samples[i] = -127;
-      }
-      Serial.println(raw_samples[i]);
-    }
-
     xSemaphoreTake(fft_mic_data_mtx, portMAX_DELAY);
     for(int i = 0; i < NUM_SAMPLES; i++){
-      mic_data_fft_copy[i] = (char)raw_samples[i];
+      mic_data_fft_copy[i] = raw_samples[i];
     }
     xSemaphoreGive(fft_mic_data_mtx);
 
